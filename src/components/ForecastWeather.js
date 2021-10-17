95bc54eab605b96c6414c7887c29382fd3b48067
Carolina Arvidsson <carolinaarvidsson_@live.se>
2021-10-17 12:57:56 +0200

    testelitest

--- src/components/ForecastWeather.js
+++ src/components/ForecastWeather.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 
 const ForecastWeather = ({ forecast }) => {
+    console.log(forecast);
     return (
         <div className='forecast-wrapper'>
             <div className='forecast'>
-
             </div>
         </div>
     );
--- src/getWeather.js
+++ src/getWeather.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const URLcurrent = 'https://api.openweathermap.org/data/2.5/weather';
-const URLforecast = 'https://api.openweathermap.org/data/2.5/forecast';
-// const weather = `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=${APIkey}&units=metric`;
-// const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&APPID=${APIkey}&units=metric`;
+const URLforecast = 'https://api.openweathermap.org/data/2.5/forecast/';
 const API_KEY = '079e60bf630788fdcc1212311df00f12';
 
 export const getWeather = async (query) => {
@@ -23,76 +21,9 @@ export const getForecast = async (query) => {
         params: {
             q: query,
             units: 'metric',
-            APPID: API_KEY
+            exclude: 'hourly',
+            APPID: API_KEY,
         }
     });
-
     return data1;
 }
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// const Main = () => {
-//     const [weather, setWeather] = useState();
-
-//     const api_call = async e => {
-//         e.preventDefault();
-//         const API_KEY = '079e60bf630788fdcc1212311df00f12';
-//         const url = `https://api.openweathermap.org/data/2.5/weather?q=Alvesta&appid=${API_KEY}`;
-//         const request = axios.get(url);
-//         const response = await request;
-//         setWeather(response.data.main);
-//     }
-//     weather && console.log(weather)
-
-//     return (
-//         <div className='main'>
-//             <Header />
-//             <Content>
-//             </Content>
-//         </div>
-//     );
-// }
-
-// export default Main;
-
-// data: function() {
-//     return {
-//       weather: null,
-//       gotWeather: false,
-//       city: "",
-//     };
-//   },
-//   methods: {
-//     getWeather() {
-//       axios
-//       .get(`https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=metric&appid=75d52df782aadd749ed253cf966cb823`)
-//       .then(response => {
-//         this.weather = response.data;
-//         this.gotWeather = true;
-//         this.city = '';
-//       })
-
-// import React, { createContext, useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// export const fetchWeather = createContext();
-
-// export const fetchWeatherProvider = (props) => {
-//     const [data, setData] = useState();
-//     const apiKey = '079e60bf630788fdcc1212311df00f12';
-
-//     useEffect(() => {
-//         axios
-//             .get(
-//                 `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=metric&appid=${apiKey}`
-//             )
-//             .then((reponse) => setData(reponse.data))
-//             .catch((error) => console.log(error));
-//     }, [])
-
-//     return (
-
-//     )
-// }
