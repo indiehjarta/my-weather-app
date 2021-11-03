9593178dd1dddef9d77910b0e04bb08dda408caa
ArnT81 <anders_soderberg@hotmail.com>
2021-11-03 13:12:38 +0100

    added weather icons in forecast

--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -2,21 +2,23 @@ import React from 'react';
 import DayJS from 'react-dayjs';
 
 
-function Card({ data }) {
+export default function Card({ data }) {
     const { dt_txt, main: { temp, feels_like, humidity }, wind: { speed } } = data;
-    const { description } = data.weather[0];
+    const { description, icon } = data.weather[0];
     const forDayJS = dt_txt.split(" ")[0];
+    const r = Math.round;
 
     return (
         <div className={'card'}>
             <DayJS date={forDayJS} format={'dddd D MMMM'} />
-            <h4> Temp: {temp}&#176;</h4>
-            <h4> Feels like: {feels_like}</h4>
+            <h4> Temp: {r(temp)}&#176;</h4>
+            <h4> Feels like: {r(feels_like)}</h4>
             <h4> Humidity: {humidity}</h4>
-            <h4> Wind: {speed}</h4>
-            <h4> Desc: {description}</h4>
+            <h4> Wind: {r(speed)}</h4>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <h4>{description}</h4>
+                <img src={`https://openweathermap.org/img/wn/${icon}.png`} alt={description} />
+            </div>
         </div>
     )
 }
-
-export default Card;
